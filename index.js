ae645357d2aebd05dc18d997b4c8a48afb08d84a
Jamie Barton <hi_jamie@me.com>
2017-10-18 07:44:50 +0100

    Add micro-cors

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
-const server = require('apollo-server-micro');
-const {makeExecutableSchema} = require('graphql-tools');
-const {formatError} = require('apollo-errors');
+const server = require('apollo-server-micro')
+const {makeExecutableSchema} = require('graphql-tools')
+const {formatError} = require('apollo-errors')
+const cors = require('micro-cors')()
 
-const typeDefs = require('./typeDefs');
-const resolvers = require('./resolvers');
+const typeDefs = require('./typeDefs')
+const resolvers = require('./resolvers')
 
 const schema = makeExecutableSchema({
   typeDefs,
   resolvers
-});
+})
 
-module.exports = server.microGraphql({
-  formatError,
-  schema
-});
+module.exports = cors(
+  server.microGraphql({
+    formatError,
+    schema
+  })
+)
