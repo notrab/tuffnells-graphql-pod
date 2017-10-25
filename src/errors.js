10c2329dc3f7dcc9b3e0afc33f0e178ca7c62632
Jamie Barton <hi_jamie@me.com>
2017-10-25 10:23:46 +0100

    Prettier update

--- src/errors.js
+++ src/errors.js
@@ -1,13 +1,14 @@
-const {createError} = require('apollo-errors');
+const {createError} = require('apollo-errors')
 
 module.exports.NotAuthorised = createError('NotAuthorised', {
   message: 'This IP is not authorised to perform a search.'
-});
+})
 
 module.exports.InvalidPostcode = createError('InvalidPostcode', {
   message: 'Invalid Postcode entered.'
-});
+})
 
 module.exports.IncorrectPostcode = createError('IncorrectPostcode', {
-  message: 'Please check the post code matches the DELIVERY post code and try again.'
-});
+  message:
+    'Please check the post code matches the DELIVERY post code and try again.'
+})
--- src/resolvers.js
+++ src/resolvers.js
@@ -1,15 +1,15 @@
-const fetch = require('node-fetch');
-const util = require('util');
-const parseXML = util.promisify(require('xml2js').parseString);
-const {NotAuthorised, InvalidPostcode, IncorrectPostcode} = require('./errors');
-const Posty = require('postcode');
-var HttpsProxyAgent = require('https-proxy-agent');
+const fetch = require('node-fetch')
+const util = require('util')
+const parseXML = util.promisify(require('xml2js').parseString)
+const {NotAuthorised, InvalidPostcode, IncorrectPostcode} = require('./errors')
+const Posty = require('postcode')
+var HttpsProxyAgent = require('https-proxy-agent')
 
-let agent = null;
-const {QUOTAGUARDSTATIC_URL} = process.env;
+let agent = null
+const {QUOTAGUARDSTATIC_URL} = process.env
 
 if (QUOTAGUARDSTATIC_URL) {
-  agent = new HttpsProxyAgent(QUOTAGUARDSTATIC_URL);
+  agent = new HttpsProxyAgent(QUOTAGUARDSTATIC_URL)
 }
 
 module.exports = {
@@ -23,31 +23,35 @@ module.exports = {
       )
         .then(res => res.text())
         .then(xml => parseXML(xml, {trim: true, explicitArray: false}))
-        .then(data => JSON.parse(JSON.stringify(data['ArrayOfTrackingRecord']['TrackingRecord'])))
+        .then(data =>
+          JSON.parse(
+            JSON.stringify(data['ArrayOfTrackingRecord']['TrackingRecord'])
+          )
+        )
         .then(data => {
           if (data.Authorised.startsWith('Not')) {
-            throw new NotAuthorised();
+            throw new NotAuthorised()
           }
 
-          const postyCode = new Posty(Postcode);
+          const postyCode = new Posty(Postcode)
 
           if (!postyCode.valid()) {
-            throw new InvalidPostcode();
+            throw new InvalidPostcode()
           }
 
           if (data.DeliveryAddress.Postcode !== postyCode.normalise()) {
-            throw new IncorrectPostcode();
+            throw new IncorrectPostcode()
           }
 
-          const movements = data.MovementInformation.Movement;
-          const scans = data.ScanInformation.Scan;
+          const movements = data.MovementInformation.Movement
+          const scans = data.ScanInformation.Scan
 
           return {
             ...data,
             MovementInformation: [...movements],
             TimedInformation: data.TimedInformation.TimedDelivery,
             ScanInformation: [...scans]
-          };
+          }
         })
   }
-};
+}
